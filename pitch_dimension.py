1fcbd778490c3c508f497c82356d9a226fe1fa13
Alberto Alves <amp.alves@campus.fct.unl.pt>
2024-10-09 04:06:20 +0100

    feat: 3D to 2D positioning

---
+++ pitch_dimension.py
@@ -0,0 +1,40 @@
+class SoccerPitch:
+    pitch_width = 10500
+    pitch_height = 6800
+    #(...) Continuar os outros
+    
+    def __init__(self):
+        
+        self.vertices = []
+        self.label = ["A",
+                      "B",
+                      "C",
+                      "D",
+                      "E",
+                      "F",
+                      "G",
+                      "H",
+                      "I",
+                      "J",
+                      "K",
+                      "L",
+                      "M",
+                      "N",
+                      "O",
+                      "P",
+                      "Q",
+                      "R",
+                      "S",
+                      "T",
+                      "U",
+                      "V",
+                      "W",
+                      "X",
+                      "Y",
+                      "Z",
+                      "AA",
+                      "BB",
+                      "CC",
+                      "DD",
+                      "EE",
+                      "FF"]
